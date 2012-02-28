9c297efb462bf002e3bd2b508501ef2e2daf2e37
Ronny Fallas <ronnyfm@gmail.com>
2012-02-28 16:19:02 -0600

    quiz finished

--- Bank_Quiz/banking.js
+++ Bank_Quiz/banking.js
@@ -6,8 +6,41 @@ function Bank(){
 	this.getExchangeRate = function(){
 		return exchangeRate;
 	}
+	
+	this.convertToDollars = function(amount){
+		var converted = convert(amount);
+		
+		if((dollars - converted) > 0){
+			dollars = dollars - converted;
+			colones = colones + amount;
+			
+			return converted;
+		}else{
+			return "No money!";
+		}
+	}
+	
+	function convert(amount){
+		return amount / exchangeRate;
+	}
+	
+	this.printReport = function(){
+		document.write("Dollars: " + dollars);
+		document.write("Colones: " + colones);
+	}
+	
+	this.reset = function(){
+		colones = 0;
+		dollars = 50000;
+	}
 }
 
 var bank = new Bank();
 
 document.write(bank.getExchangeRate());
+bank.printReport();
+bank.convertToDollars(50);
+bank.convertToDollars(500);
+bank.printReport();
+bank.reset();
+bank.printReport();
